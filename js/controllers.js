10d08f0ab4d3cd1cf1845ec44f4e5903cd770c82
Jonathan Casey <jon_casey@me.com>
2016-03-14 11:17:10 -0600

    add route and view for create game

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,14 @@ app.controller('HomeController', function($scope) {
   $scope.greeting = "Hello World!";
 }).controller('CreateController', function($scope){
   $scope.greeting = "Hello World!";
+  $scope.gameCreated = false;
+  $scope.createGame = function(){
+    console.log('clicked', $scope.createUsername);
+    $scope.gameCreated = true;
+  }
+  $scope.joinGame = function(){
+    console.log('clicked join');
+  }
 }).controller('JoinController', function($scope){
   $scope.greeting = "Hello World!";
 }).controller('GameController', function($scope){
