0280e3ca44d0b302c5579fc02ebcd40a870e9615
Jonathan Casey <jon_casey@me.com>
2016-03-14 15:40:11 -0600

    socket connectivity to node server

--- js/controllers.js
+++ js/controllers.js
@@ -1,19 +1,29 @@
+var io = require('socket.io-client/socket.io.js');
+// var io = require('../node_modules/socket.io-client/socket.io.js');
 module.exports = angular.module('drawlol').controller('HomeController', function($scope) {
   $scope.greeting = "Hello World!";
 }).controller('CreateController', function($scope){
-  $scope.greeting = "Hello World!";
   $scope.gameCreated = false;
+  $scope.roomName = '';
+  $scope.gamePath = '';
+  $scope.host = 'http://localhost:8080'
   $scope.createGame = function(){
     console.log('clicked', $scope.createUsername);
     $scope.gameCreated = true;
+    $scope.roomName = $scope.createUsername;
+    $scope.gamePath = `/#/play/${$scope.roomName}${Date.now()}`
   }
   $scope.joinGame = function(){
     console.log('clicked join');
   }
 }).controller('JoinController', function($scope){
   $scope.greeting = "Hello World!";
-}).controller('GameController', function($scope){
-  $scope.greeting = "Hello World!";
+}).controller('GameController', function($scope, $routeParams){
+  $scope.room = $routeParams.game_id;
+  var socket = io.connect('http://localhost:8000');
+  socket.on('hello', function(data){
+    console.log(data);
+  })
 }).controller('CompleteController', function($scope){
   $scope.greeting = "Hello World!";
 });
--- js/services.js
+++ js/services.js
@@ -1,1 +1,17 @@
 module.exports = {};
+// var socket = require('phoenix-elixir').Socket;
+// var LongPoller = require('phoenix-elixir').LongPoller;
+//
+// module.exports = angular.module('drawlol').factory('SocketFactory', function(room, username){
+//
+//   socket.connect();
+//   let channel = socket.channel("rooms: lobby", {});
+//   channel.join()
+//     .receive("ok", resp => {
+//       console.log("joined successfully", resp)
+//     .receive("error", resp => {
+//       console.log("Unable to join", resp);
+//     })
+//     })
+//
+// })
