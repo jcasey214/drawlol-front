bb9503c2eb825b00934febfb15a478bf6d2365b1
Jonathan Casey <jon_casey@me.com>
2016-03-14 18:56:51 -0600

    start building game view/controller

--- js/controllers.js
+++ js/controllers.js
@@ -2,15 +2,17 @@ var io = require('socket.io-client/socket.io.js');
 // var io = require('../node_modules/socket.io-client/socket.io.js');
 module.exports = angular.module('drawlol').controller('HomeController', function($scope) {
   $scope.greeting = "Hello World!";
-}).controller('CreateController', function($scope){
+}).controller('CreateController', function($scope, UsernameFactory){
   $scope.gameCreated = false;
   $scope.roomName = '';
   $scope.gamePath = '';
+  $scope.username = null;
   $scope.host = 'http://localhost:8080'
   $scope.createGame = function(){
     console.log('clicked', $scope.createUsername);
     $scope.gameCreated = true;
     $scope.roomName = $scope.createUsername;
+    UsernameFactory.setUsername($scope.createUsername);
     $scope.gamePath = `/#/play/${$scope.roomName}${Date.now()}`
   }
   $scope.joinGame = function(){
@@ -18,12 +20,30 @@ module.exports = angular.module('drawlol').controller('HomeController', function
   }
 }).controller('JoinController', function($scope){
   $scope.greeting = "Hello World!";
-}).controller('GameController', function($scope, $routeParams){
+}).controller('GameController', function($scope, $routeParams, UsernameFactory){
+  $scope.username = UsernameFactory.getUsername();
   $scope.room = $routeParams.game_id;
+  $scope.assignUsername = function(){
+    $scope.username = $scope.createUsername;
+  }
   var socket = io.connect('http://localhost:8000');
   socket.on('hello', function(data){
     console.log(data);
-  })
+    console.log('this is the username', $scope.username);
+    socket.emit('room', {roomName : $scope.room});
+  });
+  socket.on('welcome', function(data){
+    console.log(data);
+  });
+  socket.on('wtf', function(data){
+    console.log('wtf',data.message)
+  });
+  $scope.sayHello = function(){
+    socket.emit('greeting', {message: 'hello', room: $scope.room, user: $scope.username})
+  };
+  $scope.$on('$destroy', function(){
+    socket.emit('bailsor', {room: $scope.room, bailed: true})
+  });
 }).controller('CompleteController', function($scope){
   $scope.greeting = "Hello World!";
 });
--- js/services.js
+++ js/services.js
@@ -1,17 +1,11 @@
-module.exports = {};
-// var socket = require('phoenix-elixir').Socket;
-// var LongPoller = require('phoenix-elixir').LongPoller;
-//
-// module.exports = angular.module('drawlol').factory('SocketFactory', function(room, username){
-//
-//   socket.connect();
-//   let channel = socket.channel("rooms: lobby", {});
-//   channel.join()
-//     .receive("ok", resp => {
-//       console.log("joined successfully", resp)
-//     .receive("error", resp => {
-//       console.log("Unable to join", resp);
-//     })
-//     })
-//
-// })
+module.exports = angular.module('drawlol').factory('UsernameFactory', function(){
+  return {
+    username: null,
+    setUsername: function(username){
+      this.username = username;
+    },
+    getUsername: function(){
+      return this.username;
+    }
+  }
+})
