16fc3ee7e6d25d8fb5460d9eb97b6429c95efa26
Jonathan Casey <jon_casey@me.com>
2016-03-15 14:47:00 -0600

    add text input box to game play view

--- js/directives.js
+++ js/directives.js
@@ -6,11 +6,11 @@ angular.module('drawlol').directive('draw', function($document) {
     link : function(scope){
       var canvas = window._canvas = new fabric.Canvas('c');
       canvas.setHeight(600);
-      canvas.setWidth(600);
+      canvas.setWidth(800);
       canvas.isDrawingMode = true;
       canvas.backgroundColor = '#ffffff';
       canvas.freeDrawingBrush.color = '#000000';
-      canvas.freeDrawingBrush.width = 5;
+      canvas.freeDrawingBrush.width = 3;
       canvas.renderAll();
       scope.canvas = canvas;
     }
