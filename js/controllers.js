5c063188f54dac07adc45942b2e2deb0a43cdae2
Jonathan Casey <jon_casey@me.com>
2016-03-16 17:26:01 -0600

    update game controller to join socket room, emit fist sentence and start game

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,5 @@
 var io = require('socket.io-client/socket.io.js');
-// var io = require('../node_modules/socket.io-client/socket.io.js');
+
 module.exports = angular.module('drawlol').controller('HomeController', function($scope) {
   $scope.greeting = "Hello World!";
 }).controller('CreateController', function($scope, UsernameFactory){
@@ -7,7 +7,7 @@ module.exports = angular.module('drawlol').controller('HomeController', function
   $scope.roomName = '';
   $scope.gamePath = '';
   $scope.username = null;
-  $scope.host = 'http://localhost:8080'
+  $scope.host = window.location.origin;
   $scope.createGame = function(){
     console.log('clicked', $scope.createUsername);
     $scope.gameCreated = true;
@@ -18,41 +18,104 @@ module.exports = angular.module('drawlol').controller('HomeController', function
   $scope.joinGame = function(){
     console.log('clicked join');
   }
-}).controller('JoinController', function($scope){
-  $scope.greeting = "Hello World!";
-}).controller('GameController', function($scope, $routeParams, UsernameFactory, $document){
+}).controller('GameController', function($scope, $routeParams, UsernameFactory, $document, $http){
   $scope.username = UsernameFactory.getUsername();
   $scope.room = $routeParams.game_id;
+  $scope.players = [];
+  $scope.creator = false;
+  $scope.showStartButton = false;
+  $scope.round = 0;
+  $scope.currentSheet = $scope.username;
+  $scope.gameInProgress = false;
+  $scope.phase = 'draw';
+  $scope.allowSubmit = false;
+  var socket;
   $scope.assignUsername = function(){
     $scope.username = $scope.createUsername;
+    $scope.currentSheet = $scope.username
+    socket = io.connect('http://localhost:8000');
+    socket.on('handshake', function(data){
+      console.log(data);
+      console.log('this is the username', $scope.username);
+      socket.emit('joinRoom', {roomName : $scope.room, user: $scope.username});
+    });
+    socket.on('joinSuccess', function(data){
+      console.log(data);
+    });
   }
-  var socket = io.connect('http://localhost:8000');
-  socket.on('hello', function(data){
+  socket = io.connect('http://localhost:8000');
+  socket.on('handshake', function(data){
     console.log(data);
     console.log('this is the username', $scope.username);
-    socket.emit('room', {roomName : $scope.room});
+    socket.emit('joinRoom', {roomName : $scope.room, user: $scope.username});
   });
-  socket.on('welcome', function(data){
+  socket.on('joinSuccess', function(data){
     console.log(data);
+    if(data.creator){
+      $scope.creator = true;
+      $scope.showStartButton = true;
+      $scope.$digest();
+    }
   });
-  socket.on('wtf', function(data){
-    console.log('wtf',data.message)
+  socket.on('userJoined', function(data){
+    $scope.players.push(data.user);
   });
-  $scope.sayHello = function(){
-    socket.emit('greeting', {message: 'hello', room: $scope.room, user: $scope.username})
+  socket.on('success', function(data){
+    console.log('entry submitted successfully');
+    $scope.allowSubmit = false;
+    $scope.$apply();
+  })
+  $scope.chat = function(){
+    socket.emit('chatMessage', {message: 'hello', room: $scope.room, user: $scope.username})
   };
   $scope.$on('$destroy', function(){
-    socket.emit('bailsor', {room: $scope.room, bailed: true})
+    socket.emit('leaveRoom', {room: $scope.room, user: $scope.username, bailed: true})
   });
+  $scope.startGame = function(){
+    console.log('start game clicked');
+    socket.emit('start', {start: true, room: $scope.room})
+  }
+  socket.on('startGame', function(data){
+    $scope.drawCanvas.clear();
+    $scope.gameInProgress = true;
+    $scope.round = 1;
+    $scope.showStartButton = false;
+    $scope.phase = 'view';
+    $scope.allowSubmit = true;
+    $scope.$apply();
+    console.log($scope);
+    var text = new fabric.Text('The game has started \ntype in a sentence and click submit\nto start your sheet', { left: 100, top: 100, fontFamily: 'Arial' });
+    $scope.viewCanvas.add(text);
+
+  });
+  socket.on('nextRound', function(data){
+    console.log('next round', data);
+    $scope.round = data.round;
+    if($scope.phase == 'view'){
+      $scope.phase = 'draw';
+    }else if($scope.phase == 'draw'){
+      $scope.phase = 'view';
+    }
+    console.log('update next round', $scope.round);
+    $scope.allowSubmit = true;
+    $scope.$digest();
+  })
   $scope.send = function(){
     console.log("send");
-    console.log($scope.canvas);
-    var svg = $scope.canvas.toSVG({suppressPreamble: true});
-    console.log(svg);
+    var svg = $scope.drawCanvas.toSVG({suppressPreamble: true});
+    var sentence = $scope.sentence;
+    socket.emit('sheetSubmit',{
+      sheet: $scope.currentSheet,
+      phase: $scope.phase,
+      round: $scope.round,
+      roomName: $scope.room,
+      user: $scope.username,
+      sentence: sentence,
+      image: svg
+    })
+    $scope.allowSubmit = false;
+    console.log(sentence);
   }
-}).controller('CanvasController', function($scope, UsernameFactory){
-  $scope.username = UsernameFactory.getUsername();
-  $scope.canvas = new fabric.Canvas('drawingCanvas');
 }).controller('CompleteController', function($scope){
   $scope.greeting = "Hello World!";
 });
--- js/directives.js
+++ js/directives.js
@@ -1,9 +1,10 @@
-angular.module('drawlol').directive('draw', function($document) {
+angular.module('drawlol')
+.directive('draw', function() {
   return {
     restrict: 'A',
-    transclude: true,
     scope: false,
     link : function(scope){
+      console.log('draw', scope);
       var canvas = window._canvas = new fabric.Canvas('c');
       canvas.setHeight(600);
       canvas.setWidth(800);
@@ -11,8 +12,25 @@ angular.module('drawlol').directive('draw', function($document) {
       canvas.backgroundColor = '#ffffff';
       canvas.freeDrawingBrush.color = '#000000';
       canvas.freeDrawingBrush.width = 3;
+      scope.$parent.drawCanvas = canvas;
       canvas.renderAll();
-      scope.canvas = canvas;
     }
   };
-});
+}).directive('view', function($document) {
+  return {
+    restrict: 'A',
+    scope: false,
+    link : function(scope){
+      console.log('view', scope);
+      var canvas = window._canvas = new fabric.Canvas('d');
+      canvas.setHeight(600);
+      canvas.setWidth(800);
+      canvas.isDrawingMode = false;
+      canvas.backgroundColor = '#ffffff';
+      canvas.freeDrawingBrush.color = '#000000';
+      canvas.freeDrawingBrush.width = 3;
+      scope.$parent.viewCanvas = canvas;
+      canvas.renderAll();
+    }
+  };
+})
